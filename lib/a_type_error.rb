9272ba183d44aee14fb2ac7e84788dc0dc822d3c
Eric Smith <eric.c.smith@sbcglobal.net>
2020-09-12 19:45:11 +0000

    Automatically backed up by Learn

--- lib/a_type_error.rb
+++ lib/a_type_error.rb
@@ -2,4 +2,4 @@
 
 #1 + "is the loneliest number" --original error code 
 
-1.to_s + "is the loneliest number" --error fixed by converting interger to string, note adding .to_s did not work as expected
+1.to_s + "is the loneliest number" #--error fixed by converting interger to string
